11bd1d78e51a3bbce116b1bfd0dc8c3dd5c5c0f3
Kristiansito <kristianpro498@gmail.com>
2023-10-17 19:44:26 +0200

    Pequeños arreglos ortográficos

--- js/aspecto.js
+++ js/aspecto.js
@@ -2,10 +2,15 @@
 const themeCheckbox = document.getElementById("themeCheckbox");
 
 themeCheckbox.addEventListener("change", function() {
+    // Obtén el elemento <a> dentro de la sección con ID "contacto"
+    const contactoLink = document.querySelector("#contacto a");
+
     // Cambia el tema de la página según el estado del checkbox
     if (themeCheckbox.checked) {
         document.body.classList.add("dark-mode");
+        contactoLink.classList.add("dark-mode");
     } else {
         document.body.classList.remove("dark-mode");
+        contactoLink.classList.remove("dark-mode");
     }
 });
