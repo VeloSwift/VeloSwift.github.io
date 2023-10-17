d8071e85711a07c6a12f3c131479db3d0a43150a
Kristiansito <kristianpro498@gmail.com>
2023-10-17 23:10:48 +0200

    GRANDES CAMBIOS, Imagen cambia según tema

--- js/aspecto.js
+++ js/aspecto.js
@@ -2,15 +2,19 @@
 const themeCheckbox = document.getElementById("themeCheckbox");
 
 themeCheckbox.addEventListener("change", function() {
-    // Obtén el elemento <a> dentro de la sección con ID "contacto"
+    
     const contactoLink = document.querySelector("#contacto a");
+    const bannerImage = document.getElementById("bannerImage");
 
     // Cambia el tema de la página según el estado del checkbox
     if (themeCheckbox.checked) {
         document.body.classList.add("dark-mode");
         contactoLink.classList.add("dark-mode");
+        bannerImage.src = "/img/banner_night.jpg";
+        bannerImage.style.transition = "0.3s";
     } else {
         document.body.classList.remove("dark-mode");
         contactoLink.classList.remove("dark-mode");
+        bannerImage.src = "/img/banner_day.jpg";
     }
 });
